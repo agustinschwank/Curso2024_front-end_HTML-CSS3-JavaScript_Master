149a3c798a662b0f7a2609642d8a101339dadb90
Agustin Schwank <agustinschwank@hotmail.es>
2024-08-16 12:58:19 -0300

    Update main.js

--- 03-Ejercicios_de_JS/Ejercicio_23/assets/js/main.js
+++ 03-Ejercicios_de_JS/Ejercicio_23/assets/js/main.js
@@ -2,9 +2,33 @@
 
 addEventListener('DOMContentLoaded', () => {
 
-   
-
+    let input = document.querySelector('.main__input');
+    let main = document.querySelector('.layout__main');
+    let check = document.querySelector('.icon_check');
+    let xmark = document.querySelector('.icon_xmark');
 
 
     
+    input.addEventListener("keyup", (e)=>{
+        validateMail();
+    });
+
+
+    let validateMail =  () => {
+        let email = input.value;
+        let pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (email.trim() != '') {
+            if(pattern.test(email)){
+                check.style.opacity = 1;
+                xmark.style.opacity = 0;
+                main.style.borderColor = '#0bd815';
+            } else {
+                xmark.style.opacity = 1;
+                check.style.opacity = 0;
+                main.style.borderColor = '#f83535';
+            }
+        }
+    }  
+
+
 });
