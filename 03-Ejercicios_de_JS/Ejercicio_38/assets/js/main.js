4d1c963d10d03f2fbfefdf74a0f3f1642cf4a981
Agustin Schwank <agustinschwank@hotmail.es>
2024-09-05 12:39:08 -0300

    upgrade

--- 03-Ejercicios_de_JS/Ejercicio_38/assets/js/main.js
+++ 03-Ejercicios_de_JS/Ejercicio_38/assets/js/main.js
@@ -5,7 +5,7 @@ window.addEventListener("DOMContentLoaded", () => {
     let audios = document.querySelectorAll(".sounds__audio");
     let btnPlay = document.querySelectorAll(".sounds__btn");
 
-    let start = () => {
+   /* let start = () => {
         btnPlay.forEach((play) => {
             play.addEventListener("click", ()=>{
                 let dataID = play.getAttribute("data-audio");
@@ -14,16 +14,32 @@ window.addEventListener("DOMContentLoaded", () => {
                 audio.play();
             });
         });
-    }
+    }*/
 
-    let stop = () => {
+
+
+        // Esto es una IIFE: Funcion de ejecucion inmediata y anonima
+    (() =>  {
+                btnPlay.forEach((play) => {
+                    play.addEventListener("click", ()=>{
+                        let dataID = play.getAttribute("data-audio");
+                        let audio = document.querySelector("#"+dataID);
+                        stop();
+                        audio.play();
+                    });
+                });
+            })();
+
+
+
+    const stop = () => {
         audios.forEach((audio) =>{
             audio.pause();
             audio.currentTime = 0;
         });
     }
 
-    start();
+    //start();
     
 
 });
